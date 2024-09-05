9d73a84cd905fda940e32f844777c805ee3c9e88
anna-vasylenko <anna.bilych16@gmail.com>
2024-09-06 01:00:16 +0300

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./auth/slice";
 import { transactionsReducer } from "./transaction/slice";
-import { filtersReducer } from "./filters/slice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     transaction: transactionsReducer,
-    filter: filtersReducer,
   },
 });
