80343763b16d1a5b61c360d5af866ce5da7ef0fa
anna-vasylenko <anna.bilych16@gmail.com>
2024-09-05 20:54:48 +0300

    add routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,54 @@
+import { Suspense } from "react";
 import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+// import Layout from "../Layout/Layout";
+import Loader from "../Loader/Loader";
+import DashboardPage from "../../pages/DashboardPage/DashboardPage";
+import RegistrationPage from "../../pages/RegistrationPage/RegistrationPage";
+import LoginPage from "../../pages/LoginPage/LoginPage";
+import HomeTab from "../../pages/HomeTab/HomeTab";
+import CurrencyTab from "../../pages/CurrencyTab/CurrencyTab";
+import StatisticsTab from "../../pages/StatisticsTab/StatisticsTab";
 
 function App() {
-  return <></>;
+  return (
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            // <PrivateRoute>
+            <DashboardPage />
+            // </PrivateRoute>
+          }
+        >
+          <Route index element={<HomeTab />} />
+          <Route path="/currency" element={<CurrencyTab />} />
+          <Route path="/statistics" element={<StatisticsTab />} />
+        </Route>
+
+        <Route
+          path="register"
+          element={
+            // <RestrictedRoute>
+            <RegistrationPage />
+            // </RestrictedRoute>
+          }
+        />
+
+        <Route
+          path="login"
+          element={
+            // <RestrictedRoute>
+            <LoginPage />
+            // </RestrictedRoute>
+          }
+        />
+
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Suspense>
+  );
 }
 
 export default App;
