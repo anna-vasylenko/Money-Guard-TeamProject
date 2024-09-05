08bafcf18a1d56b4244f37499085e8b884f5d0aa
Anna Vasylenko <166040592+anna-vasylenko@users.noreply.github.com>
2024-09-05 21:21:40 +0300

    Add base/anna/05 09 24 (#4)
    * Update operations.js
    
    * Update selectors.js
    
    * add routes
    
    * add authSlice

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,11 +1,65 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, isAnyOf } from "@reduxjs/toolkit";
+import {
+  loginThunk,
+  logoutThunk,
+  refreshUserThunk,
+  registerThunk,
+} from "./operations";
 
-const initialState = {};
+const initialState = {
+  user: {
+    name: null,
+    email: null,
+  },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+};
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerThunk.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+        state.isError = null;
+      })
+      .addCase(loginThunk.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+        state.isError = null;
+      })
+      .addCase(logoutThunk.fulfilled, () => {
+        return initialState;
+      })
+      .addCase(refreshUserThunk.pending, (state) => {
+        state.isRefreshing = true;
+      })
+      .addCase(refreshUserThunk.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(refreshUserThunk.rejected, (state) => {
+        state.isRefreshing = false;
+      })
+      .addMatcher(
+        isAnyOf(registerThunk.rejected, loginThunk.rejected),
+        (state, action) => {
+          state.isError = action.payload;
+        }
+      )
+      .addMatcher(
+        isAnyOf(registerThunk.pending, loginThunk.pending),
+        (state) => {
+          state.isError = false;
+        }
+      );
+  },
 });
 
 export const authReducer = authSlice.reducer;
