aa7018d1c59fd445b486e98c055cea6cdcdd5c11
Anna Vasylenko <166040592+anna-vasylenko@users.noreply.github.com>
2024-09-05 23:14:22 +0300

    Update operations.js (#6)

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = "https://wallet.b.goit.study/";
+axios.defaults.baseURL = "https://wallet.b.goit.study";
 
 const setAuthHeader = (token) => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
