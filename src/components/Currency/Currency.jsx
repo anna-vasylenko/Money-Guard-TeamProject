5bcdf9db9940d70ae6019de6ebfcd8429b11dc7d
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-09-05 19:10:31 +0200

    Update Currency.jsx

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -1,5 +1,30 @@
+import { useDispatch } from "react-redux";
+
 const Currency = () => {
-  return <div>Currency</div>;
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <ul>
+        <li>
+          <h3>Currency</h3>
+          <h3>Purchase</h3>
+          <h3>Sale</h3>
+        </li>
+        <li>
+          <p>USD</p>
+          <p>`${}`</p>
+          <p>`${}`</p>
+        </li>
+        <li>
+          <p>EUR</p>
+          <p>`${}`</p>
+          <p>`${}`</p>
+        </li>
+      </ul>
+      <img src="" alt="" />
+    </div>
+  );
 };
 
 export default Currency;
