be9daefd12717b79076d9bc6e65268b423e9d7fe
Julia <jpkoord@gmail.com>
2024-09-05 21:06:16 +0300

    reg

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,5 +1,77 @@
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
+import s from './RegistrationForm.module.css';
+import { useDispatch } from 'react-redux';
+import { registration } from '../../redux/auth/slice';
+
 const RegistrationForm = () => {
-  return <div>RegistrationForm</div>;
+  const dispatch = useDispatch();
+  const onSubmit = ({ email, name, password }, { resetForm }) => {
+    dispatch(registration({ email, name, password }));
+    resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={{
+        email: '',
+        name: '',
+        password: '',
+      }}
+      validationSchema={Yup.object({
+        name: Yup.string()
+          .min(2, 'Minimum length is 2 characters')
+          .max(20, 'Maximum length is 20 characters')
+          .required('Required'),
+        email: Yup.string().email('Invalid email address').required('Required'),
+        password: Yup.string()
+          .min(6, 'Password must be at least 6 characters long')
+          .max(12, 'Password cannot exceed 12 characters')
+          .required('Required'),
+        confirmPassword: Yup.string()
+          .oneOf([Yup.ref('password'), null], 'Passwords must match')
+          .required('Required'),
+      })}
+      onSubmit={onSubmit}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <p>Money Guard</p>
+          <div>
+            <Field type="email" name="email" placeholder="E-mail" />
+            <ErrorMessage name="email" component="div" />
+          </div>
+          <div>
+            <Field type="text" name="name" placeholder="Name" />
+            <ErrorMessage name="name" component="div" />
+          </div>
+          <div>
+            <Field type="password" name="password" placeholder="Password" />
+            <ErrorMessage name="password" component="div" />
+          </div>
+          <div>
+            <Field
+              type="password"
+              name="confirmPassword"
+              placeholder="Confirm Password"
+            />
+            <ErrorMessage name="confirmPassword" component="div" />
+          </div>
+          <button
+            disabled={isSubmitting}
+            type="submit"
+            className={s.button_reg}
+          >
+            Registration
+          </button>
+          <Link to="/login">
+            <button type="button">Login</button>
+          </Link>
+        </Form>
+      )}
+    </Formik>
+  );
 };
 
 export default RegistrationForm;
