eb3c233ca5adb9f23f8aa1be36d0b87fe366376f
anna-vasylenko <anna.bilych16@gmail.com>
2024-09-05 23:05:00 +0300

    update

--- src/redux/transaction/operations.js
+++ src/redux/transaction/operations.js
@@ -1,2 +1,59 @@
-import axios from "axios";
-import { createAsyncThunk } from "@reduxjs/toolkit";
+// import axios from "axios";
+// import { createAsyncThunk } from "@reduxjs/toolkit";
+
+// export const fetchTransactions = createAsyncThunk(
+//   "transactions/fetchAll",
+//   async (_, thunkAPI) => {
+//     try {
+//       const { data } = await axios.get("/contacts");
+//       return data;
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error.message);
+//     }
+//   }
+// );
+
+// export const addTransaction = createAsyncThunk(
+//   "transactions/addContact",
+//   async (contact, thunkAPI) => {
+//     try {
+//       const { data } = await axios.post("/contacts", contact);
+//       return data;
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error.message);
+//     }
+//   }
+// );
+
+// export const deleteTransaction = createAsyncThunk(
+//   "transactions/deleteContact",
+//   async (id, thunkAPI) => {
+//     try {
+//       const { data } = await axios.delete(`/contacts/${id}`);
+//       return data.id;
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error.message);
+//     }
+//   },
+//   {
+//     condition: (_, thunkAPI) => {
+//       const isLoading = thunkAPI.getState().contacts.loading;
+//       if (isLoading) {
+//         return false;
+//       }
+//     },
+//   }
+// );
+
+// export const updateTransaction = createAsyncThunk(
+//   "transactions/updateContact",
+//   async ({ name, number, id }, thunkApi) => {
+//     try {
+//       const { data } = await axios.patch(`/contacts/${id}`, { name, number });
+
+//       return data;
+//     } catch (error) {
+//       return thunkApi.rejectWithValue(error.message);
+//     }
+//   }
+// );
--- src/redux/transaction/slice.js
+++ src/redux/transaction/slice.js
@@ -1,11 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
+const initialState = {
+  transactions: [],
+  periodTransactions: [],
+  isLoading: false,
+  isError: null,
+  currentTransaction: null,
+  categories: [],
+};
 
 const transactionsSlice = createSlice({
   name: "transactions",
   initialState,
-  reducers: {},
+  reducers: {
+    currentTransaction(state, action) {
+      state.currentContact = action.payload;
+    },
+  },
 });
 
 export const transactionsReducer = transactionsSlice.reducer;
