60ee489bc206898315f11ec76fc90b6722114129
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-09-05 21:26:53 +0200

    Summary (required)

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -1,7 +1,22 @@
 import { useDispatch } from "react-redux";
+import React from "react";
+import {
+  AreaChart,
+  Area,
+  Tooltip,
+  ResponsiveContainer,
+  LabelList,
+} from "recharts";
 
 const Currency = () => {
-  const dispatch = useDispatch();
+  // Дані для графіків
+  const data = [
+    { name: "x", value: 10 },
+    { name: "USD", value: 17.55 },
+    { name: "x", value: 10 },
+    { name: "EURO", value: 30.0 },
+    { name: "x", value: 8 },
+  ];
 
   return (
     <div>
@@ -13,16 +28,59 @@ const Currency = () => {
         </li>
         <li>
           <p>USD</p>
-          <p>`${}`</p>
-          <p>`${}`</p>
+          <p>{`${data[1].value}`}</p> {/* Відображення курсу валюти */}
+          <p>{`${data[1].value}`}</p>
         </li>
         <li>
           <p>EUR</p>
-          <p>`${}`</p>
-          <p>`${}`</p>
+          <p>{`${data[3].value}`}</p> {/* Відображення курсу валюти */}
+          <p>{`${data[3].value}`}</p>
         </li>
       </ul>
-      <img src="" alt="" />
+
+      {/* Графіки */}
+      <div
+        style={{
+          width: "100%",
+          height: "300px",
+          backgroundColor: "#2b1a64",
+          position: "relative",
+          marginTop: "15px", // Відступ між графіками
+        }}
+      >
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart data={data} className="custom-chart">
+            {/* Нижній графік, що повторює форму верхньої кривої */}
+            <Area
+              type="monotone"
+              dataKey="value"
+              stroke="none"
+              fill="rgba(255, 255, 255, 0.2)"
+              fillOpacity={1}
+              transform="translate(0, 20)"
+            />
+            {/* Верхній графік, що відображає курс валют */}
+            <Area
+              type="monotone"
+              dataKey="value"
+              stroke="#ff6f61"
+              strokeWidth={2}
+              fill="none"
+              activeDot={{ r: 4, fill: "#ff6f61" }}
+            >
+              {/* Використання LabelList для відображення значень */}
+              <LabelList
+                dataKey="value"
+                position="top"
+                offset={10}
+                fill="#ff6f61"
+                fontSize={14}
+              />
+            </Area>
+            <Tooltip />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
