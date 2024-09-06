b9c929655bbd46188fab1f47e5fa87eaf0a591a2
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-09-06 17:19:29 +0200

    Update Currency.jsx

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { useDispatch, useSelector } from "react-redux";
 import React, { useEffect, useState } from "react";
 import {
@@ -111,6 +112,33 @@ const Currency = () => {
           </AreaChart>
         </ResponsiveContainer>
       </div>
+=======
+import { useDispatch } from "react-redux";
+
+const Currency = () => {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <ul>
+        <li>
+          <h3>Currency</h3>
+          <h3>Purchase</h3>
+          <h3>Sale</h3>
+        </li>
+        <li>
+          <p>USD</p>
+          <p>`${}`</p>
+          <p>`${}`</p>
+        </li>
+        <li>
+          <p>EUR</p>
+          <p>`${}`</p>
+          <p>`${}`</p>
+        </li>
+      </ul>
+
+      <img src="" alt="" />
+>>>>>>> Stashed changes
     </div>
   );
 };
