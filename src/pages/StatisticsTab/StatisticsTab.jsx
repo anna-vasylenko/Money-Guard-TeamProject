6cde238b6de2fc1a139ba48fbf92d3b933ec4b77
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-09-06 14:30:28 +0300

    Add useEffect and child

--- src/pages/StatisticsTab/StatisticsTab.jsx
+++ src/pages/StatisticsTab/StatisticsTab.jsx
@@ -1,5 +1,25 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Chart } from "react-chartjs-2";
+
+import StatisticsDashboard from "../../components/StatisticsDashboard/StatisticsDashboard";
+import StatisticsTable from "../../components/StatisticsTable/StatisticsTable";
+import { getPeriodTransactions } from "../../redux/transaction/operations";
+
 const StatisticsTab = () => {
-  return <div>StatisticsTab</div>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPeriodTransactions);
+  }, [dispatch]);
+
+  return (
+    <div>
+      <Chart />
+      <StatisticsDashboard />
+      <StatisticsTable />
+    </div>
+  );
 };
 
 export default StatisticsTab;
