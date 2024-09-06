cd6a818bc48e0b3bda0281716241b2fcae067f4a
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-09-06 14:37:08 +0300

    Delete useEffect

--- src/pages/StatisticsTab/StatisticsTab.jsx
+++ src/pages/StatisticsTab/StatisticsTab.jsx
@@ -1,17 +1,9 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import { Chart } from "react-chartjs-2";
 
 import StatisticsDashboard from "../../components/StatisticsDashboard/StatisticsDashboard";
 import StatisticsTable from "../../components/StatisticsTable/StatisticsTable";
-import { getPeriodTransactions } from "../../redux/transaction/operations";
 
 const StatisticsTab = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPeriodTransactions);
-  }, [dispatch]);
 
   return (
     <div>
