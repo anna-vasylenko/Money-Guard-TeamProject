d6308be4959e69bbe5020bdbeb6e9b45c604e136
dekizber <dekizber@gmail.com>
2024-09-06 23:49:18 +0300

    save

--- src/components/TransactionsList/TransactionsList.jsx
+++ src/components/TransactionsList/TransactionsList.jsx
@@ -1,47 +1,49 @@
-// import { useSelector } from "react-redux";
 import s from "./TransactionsList.module.css";
 import TransactionsItem from "../TransactionsItem/TransactionsItem";
+import { useSelector } from "react-redux";
+import { selectTransactions } from "../../redux/transaction/selectors";
 
-const transactions = [
-  {
-    date: "04.01.23",
-    type: "-",
-    category: "Other",
-    comment: "Gift for your wife",
-    sum: 300.0,
-  },
-  {
-    date: "05.01.23",
-    type: "+",
-    category: "Income",
-    comment: "January bonus",
-    sum: 8000.0,
-  },
-  {
-    date: "07.01.23",
-    type: "-",
-    category: "Car",
-    comment: "Oil",
-    sum: 1000.0,
-  },
-  {
-    date: "07.01.23",
-    type: "-",
-    category: "Products",
-    comment: "Vegetables for the week",
-    sum: 280.0,
-  },
-  {
-    date: "07.01.23",
-    type: "+",
-    category: "Income",
-    comment: "Gift",
-    sum: 1000.0,
-  },
-];
+// const transactions = [
+//   {
+//     date: "04.01.23",
+//     type: "-",
+//     category: "Other",
+//     comment: "Gift for your wife",
+//     sum: 300.0,
+//   },
+//   {
+//     date: "05.01.23",
+//     type: "+",
+//     category: "Income",
+//     comment: "January bonus",
+//     sum: 8000.0,
+//   },
+//   {
+//     date: "07.01.23",
+//     type: "-",
+//     category: "Car",
+//     comment: "Oil",
+//     sum: 1000.0,
+//   },
+//   {
+//     date: "07.01.23",
+//     type: "-",
+//     category: "Products",
+//     comment: "Vegetables for the week",
+//     sum: 280.0,
+//   },
+//   {
+//     date: "07.01.23",
+//     type: "+",
+//     category: "Income",
+//     comment: "Gift",
+//     sum: 1000.0,
+//   },
+// ];
 
 const TransactionsList = () => {
-  // const transactionData = useSelector();
+  const transactions = useSelector(selectTransactions);
+
   return (
     <div className={s.financeTableContainer}>
       <table className={s.financeTable}>
