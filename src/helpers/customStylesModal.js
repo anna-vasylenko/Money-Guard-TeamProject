fd5e9a2ce5754f7b087f658264c871fec83a4efe
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-09-07 22:54:56 +0300

    Add style for Modal and add file js for customStyles

---
+++ src/helpers/customStylesModal.js
@@ -0,0 +1,24 @@
+export const customStyles = {
+  overlay: {
+    backgroundColor: "rgba(34, 13, 91, 0.23)",
+    backdropFilter: "blur(3.5px)",
+  },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    padding: 20,
+    width: "540px",
+    height: "589px",
+    maxWidth: "90vw",
+    maxHeight: "90vh",
+    border: "none",
+    borderRadius: "8px",
+    backgroundColor: "var(--form-color)",
+    boxShadow: "0px 4px 60px 0px rgba(0, 0, 0, 0.25)",
+    backdropFilter: "blur(50px)",
+  },
+};
