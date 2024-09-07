040cb0413503ea5f95191523051bd3d72542c195
Nicolai Dodeac <Mykola.Dodiak@gmail.com>
2024-09-07 09:37:18 +0100

    done

--- src/components/Icons/Icons.jsx
+++ src/components/Icons/Icons.jsx
@@ -1,5 +1,5 @@
-import Sprite from "../../src/images/symbol-defs.svg";
-export const Icon = ({ className, width = 12, height = 12, name }) => {
+import Sprite from "../../../src/images/symbol-defs.svg";
+export const Icons = ({ className, width = 12, height = 12, name }) => {
   return (
     <svg className={className} width={width} height={height} stroke="#fafafa">
       <use href={`${Sprite}#${name}`}></use>
