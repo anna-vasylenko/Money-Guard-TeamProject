8bc9c7413dc9a7a87517015458fb50bd33d267f1
NicolaiDodeac <163674087+NicolaiDodeac@users.noreply.github.com>
2024-09-07 20:18:45 +0100

    Origin/statistics/nicolai d (#33)
    * halfway
    
    * summary
    
    * step
    
    * halfway
    
    * mobile
    
    * mobile
    
    * css
    
    * sprite
    
    * done
    
    * css
    
    * shadow
    
    * Dekizber
    
    * dekizber 2
    
    * update

--- src/pages/StatisticsTab/StatisticsTab.jsx
+++ src/pages/StatisticsTab/StatisticsTab.jsx
@@ -1,10 +1,12 @@
 import Chart from "../../components/Chart/Chart";
 import StatisticsDashboard from "../../components/StatisticsDashboard/StatisticsDashboard";
 import StatisticsTable from "../../components/StatisticsTable/StatisticsTable";
+import s from "./StatisticTab.module.css";
 
 const StatisticsTab = () => {
   return (
     <div>
+      <h1 className={s.header}>Statistics</h1>
       <Chart />
       <StatisticsDashboard />
       <StatisticsTable />
