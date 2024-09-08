f5bccc33639f5f8e8f607f36ade07925edd3e614
Anna Vasylenko <166040592+anna-vasylenko@users.noreply.github.com>
2024-09-08 20:11:04 +0300

    Anna/07/09 (#52)
    * Update App.jsx
    
    * update
    
    * update
    
    * update
    
    * add modal slice
    
    * container
    
    * update
    
    * update bg
    
    * Update EditTransactionForm.jsx

--- src/components/TransactionsItem/TransactionsItem.jsx
+++ src/components/TransactionsItem/TransactionsItem.jsx
@@ -2,6 +2,8 @@ import { useDispatch } from "react-redux";
 import { Icons } from "../Icons/Icons";
 import s from "./TransactionsItem.module.css";
 import { openEditModal } from "../../redux/modal/slice";
+import { setCurrentTransaction } from "../../redux/transaction/slice";
+import { deleteTransaction } from "../../redux/transaction/operations";
 
 const formatDate = (dateString) => {
   const options = { day: "2-digit", month: "2-digit", year: "2-digit" };
@@ -10,6 +12,13 @@ const formatDate = (dateString) => {
 
 const TransactionsItem = ({ transaction }) => {
   const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(openEditModal());
+    dispatch(setCurrentTransaction({ transaction }));
+  };
+  console.log(transaction);
+
   return (
     <tr>
       <td>{formatDate(transaction.transactionDate)}</td>
@@ -20,15 +29,17 @@ const TransactionsItem = ({ transaction }) => {
         {transaction.amount}
       </td>
       <td className={s.actionBtn}>
-        <button
-          onClick={() => {
-            dispatch(openEditModal());
-          }}
-          className={s.editBtn}
-        >
+        <button type={"submit"} onClick={handleClick} className={s.editBtn}>
           <Icons className={s.editIcon} name={"pencil"} />
         </button>
-        <button className={s.deleteBtn}>Delete</button>
+        <button
+          className={s.deleteBtn}
+          onClick={() => {
+            dispatch(deleteTransaction(transaction.id));
+          }}
+        >
+          Delete
+        </button>
       </td>
     </tr>
   );
