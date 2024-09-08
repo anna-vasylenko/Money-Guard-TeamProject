41032305aa7d92bf9c4c41127647e55b02fbd45b
dekizber <dekizber@gmail.com>
2024-09-08 23:02:34 +0300

    save desk ver

--- src/components/TransactionsList/TransactionsList.jsx
+++ src/components/TransactionsList/TransactionsList.jsx
@@ -2,6 +2,7 @@ import s from "./TransactionsList.module.css";
 import TransactionsItem from "../TransactionsItem/TransactionsItem";
 import { useSelector } from "react-redux";
 import { selectTransactions } from "../../redux/transaction/selectors";
+import { useMedia } from "../../hooks/useMedia";
 
 const TransactionsList = () => {
   const transactions = useSelector(selectTransactions);
@@ -9,17 +10,19 @@ const TransactionsList = () => {
     (a, b) => new Date(b.transactionDate) - new Date(a.transactionDate)
   );
 
+  const { isMobile } = useMedia();
+
   return (
     <div className={s.financeTableContainer}>
       <table className={s.financeTable}>
         <thead className={s.headTab}>
           <tr className={s.tr}>
-            <th className={s.th}>Date</th>
-            <th className={s.th}>Type</th>
-            <th className={s.th}>Category</th>
-            <th className={s.th}>Comment</th>
-            <th className={s.thSum}>Sum</th>
-            <th className={s.th}></th>
+            <th className={s.date}>Date</th>
+            <th className={s.type}>Type</th>
+            <th className={s.category}>Category</th>
+            <th className={s.comment}>Comment</th>
+            <th className={s.sum}>Sum</th>
+            <th className={s.actions}></th>
           </tr>
         </thead>
         <tbody className={s.th}>
