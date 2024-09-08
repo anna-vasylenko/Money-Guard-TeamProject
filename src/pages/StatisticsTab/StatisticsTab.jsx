30034ac59f3889ba5ea636a384945d44e470c2cb
Nicolai Dodeac <Mykola.Dodiak@gmail.com>
2024-09-08 16:29:52 +0100

    Statistics_tab

--- src/pages/StatisticsTab/StatisticsTab.jsx
+++ src/pages/StatisticsTab/StatisticsTab.jsx
@@ -5,11 +5,15 @@ import s from "./StatisticTab.module.css";
 
 const StatisticsTab = () => {
   return (
-    <div>
-      <h1 className={s.header}>Statistics</h1>
-      <Chart />
-      <StatisticsDashboard />
-      <StatisticsTable />
+    <div className={s.statisticsWrapper}>
+      <div className={s.doughnutWrapper}>
+        <h1 className={s.header}>Statistics</h1>
+        <Chart />
+      </div>
+      <div className={s.tableWrapper}>
+        <StatisticsDashboard />
+        <StatisticsTable />
+      </div>
     </div>
   );
 };
