8ee1b59f6f1fe7b2458b806a24d106ef5cd655b0
anna-vasylenko <anna.bilych16@gmail.com>
2024-09-10 01:38:54 +0300

    update

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,41 +1,18 @@
 import RegistrationForm from "../../components/RegistrationForm/RegistrationForm.jsx";
-// import s from './RegistrationPage.module.css';
-import { useDispatch } from "react-redux";
-import { registerThunk } from "../../redux/auth/operations.js";
-// import { toast } from 'react-toastify';
 import s from "./RegistrationPage.module.css";
 import { Toaster } from "react-hot-toast";
 
-const initialValues = {
-  username: "",
-  password: "",
-  email: "",
-  confirmPassword: "",
-};
 const RegistrationPage = () => {
-  const dispatch = useDispatch();
-
-  const handleSubmit = ({ username, email, password }, { resetForm }) => {
-    dispatch(registerThunk({ username, email, password })).unwrap();
-    // .then(data => {
-    //   toast.success(`Registration is success ${data.user.name}, welcome!`);
-    // })
-    // .catch(() => {
-    //   toast.error('Invalid credentials');
-    // });
-
-    resetForm();
-  };
-
   return (
     <div className={s.pageRegister}>
-      <RegistrationForm
-        type="register"
-        title="Registration"
-        onSubmit={handleSubmit}
-        initialValues={initialValues}
-      />
+      <RegistrationForm />
       <Toaster />
+      <div className={s.ellipse16}></div>
+      <div className={s.ellipse18}></div>
+      <div className={s.ellipse14}></div>
+      <div className={s.ellipse17}></div>
+      <div className={s.ellipse15}></div>
+      <div className={s.ellipse19}></div>
     </div>
   );
 };
