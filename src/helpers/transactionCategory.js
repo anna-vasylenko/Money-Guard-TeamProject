c1a6e3c5c4b1ef2e55810c538fff4ed4abfe539c
Anna Vasylenko <166040592+anna-vasylenko@users.noreply.github.com>
2024-09-09 03:45:35 +0300

    Anna/07/09 (#59)
    * Update operations.js
    
    * update
    
    * update
    
    * update

--- src/helpers/transactionCategory.js
+++ src/helpers/transactionCategory.js
@@ -1,6 +1,6 @@
 export const getTransactionCategory = (categoryId, categories) => {
   const transactionCategory = categories.find((item) => item.id === categoryId);
-
+  if (!transactionCategory) return;
   return transactionCategory.name;
 };
 
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,11 +1,16 @@
+import { Navigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../redux/auth/selectors";
-import { Navigate } from "react-router-dom";
 
-const PrivateRoute = ({ children }) => {
+const PrivateRoute = ({ component: Component }) => {
+  const location = useLocation();
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
-  return isLoggedIn ? children : <Navigate to="/login" />;
+  return isLoggedIn ? (
+    Component
+  ) : (
+    <Navigate to="/login" replace state={location} />
+  );
 };
 
 export default PrivateRoute;
