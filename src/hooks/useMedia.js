e9d54072682d73d277a02b4caa0c647285f4e316
Anna Vasylenko <166040592+anna-vasylenko@users.noreply.github.com>
2024-09-10 17:19:18 +0300

    update (#82)

--- src/hooks/useMedia.js
+++ src/hooks/useMedia.js
@@ -4,11 +4,9 @@ export const useMedia = () => {
   const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
   const isTablet = useMediaQuery({ query: "(min-width: 768px)" });
   const isDesktop = useMediaQuery({ query: "(min-width: 1280px)" });
-  const isRetina = useMediaQuery({ query: "(min-resolution: 2dppx)" });
   return {
     isMobile,
     isTablet,
     isDesktop,
-    isRetina,
   };
 };
