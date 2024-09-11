3257a617075fdd7a973c04e60d76342a9ecb851e
dekizber <dekizber@gmail.com>
2024-09-11 12:19:30 +0300

    edit valid

--- src/helpers/editValidationSchema.js
+++ src/helpers/editValidationSchema.js
@@ -1,9 +1,3 @@
-// import * as yup from "yup";
-
-// export const editValidationSchema = yup.object().shape({
-//   amount: yup.string().required("Required"),
-//   comment: yup.string().required("Required"),
-// });
 import * as yup from "yup";
 
 export const editValidationSchema = (type) => {
@@ -16,10 +10,6 @@ export const editValidationSchema = (type) => {
     comment: yup
       .string()
       .max(100, "Comment cannot exceed 100 characters")
-      .required("Comment is required"),
-    categoryId:
-      type === "Expense"
-        ? yup.string().required("Category is required")
-        : yup.string().nullable(),
+      .required("Comment is required")
   });
 };
